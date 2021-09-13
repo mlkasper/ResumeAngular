18e44069f9a707fd7021091df5306d49fceebf8b
Michael Kasper <michael@mlkasper.com>
2021-09-13 16:29:45 -0500

    update with material dependencies

--- resume-angular/src/app/app.module.ts
+++ resume-angular/src/app/app.module.ts
@@ -4,15 +4,23 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { ProjectsComponent } from './projects/projects.component';
+
+import { MatToolbarModule } from '@angular/material/toolbar'; 
+import { MatIconModule } from '@angular/material/icon';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'; 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProjectsComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    NgbModule
+    NgbModule, 
+    MatIconModule, 
+    MatToolbarModule, BrowserAnimationsModule, 
   ],
   providers: [],
   bootstrap: [AppComponent]
