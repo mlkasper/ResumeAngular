3efb01c01ca83326329f80b3081ea2af5fed5b53
Michael Kasper <michael@mlkasper.com>
2021-09-17 12:30:30 -0500

    bug fix
    bug fixed routing

--- resume-angular/src/app/app-routing.module.ts
+++ resume-angular/src/app/app-routing.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ProjectComponent } from './project/project.component';
 import { ComingSoonComponent } from './coming-soon/coming-soon.component';
+import { AppComponent } from './app.component';
 
 const routes: Routes = [
   { path: 'projects', component: ProjectComponent },
-  { path: 'home', component: ComingSoonComponent}
+  {path: '**', redirectTo: 'home', pathMatch: 'full'},
+  { path: 'home', component: ComingSoonComponent },
+  
+  
 ];
 
 @NgModule({
